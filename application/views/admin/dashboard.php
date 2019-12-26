d7d60507e8765a07456f5da6e0073374b9bcb08a
tamtam931 <zarmaynine@yahoo.com>
2019-12-26 17:39:19 +0800

    with Turnover Dashboard views/admin/dashboard.php -Incomplete 12-26-19

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,24 @@
+<?= $this->load->view('top','',TRUE) ?>
+<div class="container">
+	<div class="row">
+		<h3 class="mt-3">TURNOVER DASHBOARD</h3>
+	</div>
+	<div class="col-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="row">
+			<form action="<?= base_url('admin/dashboard') ?>" method="GET" role="form">
+				<div class="form-group">
+					<?= form_label('Project', 'project') ?>
+					<div class="input-group">
+						<?= form_dropdown('project',$projects,null,['id' => 'project', 'class' => 'form-control', 'placeholder' => 'Project List']) ?>
+						<div class="input-group-append">
+							<?= form_submit('submit','SEARCH',['class' => 'search_unit btn btn-outline-dark','id' => 'search_unit']) ?>
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+	<hr>
+	<div class="row" id="app">
+	</div>
+</div>
